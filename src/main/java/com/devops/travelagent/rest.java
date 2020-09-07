1ab11ff86d5972a09b48ea222bf0365095c35bef
shashankyadav03 <shashankyadav97@yahoo.in>
2020-09-07 13:40:28 +0530

    web upload

--- src/main/java/com/devops/travelagent/rest.java
+++ src/main/java/com/devops/travelagent/rest.java
@@ -1,19 +1,22 @@
 package com.devops.travelagent;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class rest {
-	
-	@GetMapping("/")
-	public String welcome() {
-		return "Welcome to our Travel site!";
-	}
+
+@Controller
+public class rest {   
+	@GetMapping("/")    
+	public String index()  {    
+		return"index";    
+	}    
 	
 	@GetMapping("/home")
-	public String home() {
-		return "Welcome to our Travel site!";
+	public String home(ModelMap model) {
+		return "welcome";
 	}
+	
+	   
 	
 }
