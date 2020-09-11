cf4892ca5e48fadcd5a565a11342e9af8f2dc1b7
shashankyadav03 <shashankyadav97@yahoo.in>
2020-09-11 12:10:25 +0530

    target updated

--- src/main/java/com/devops/travelagent/rest.java
+++ src/main/java/com/devops/travelagent/rest.java
@@ -1,25 +1,35 @@
 package com.devops.travelagent;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
 public class rest {   
 	@GetMapping("/")    
-	public String index()  {    
+	public String index(Model model)  {  
+		model.addAttribute("user", new user());
 		return"index";    
 	}    
-	
-	@GetMapping("/home")
-	public String home(ModelMap model) {
+	@PostMapping("welcome")//check
+    public String personSubmit(@ModelAttribute user user,Model model) {
+		System.out.println(user.username);
+		model.addAttribute("search", new search());
 		return "welcome";
-	}
+//    return "results";
+    }
 	
-	@GetMapping("/agent")
-	public String agent(ModelMap model) {
+	@PostMapping("agent")//check
+    public String personSubmit(@ModelAttribute search search) {
+		System.out.println(search.place);
 		return "agent";
-	}
+//    return "results";
+    }
+	
+	
 	
 }
---
+++ src/main/java/com/devops/travelagent/search.java
@@ -0,0 +1,16 @@
+package com.devops.travelagent;
+
+public class search {
+
+	public String place;
+
+	public String getPlace() {
+		return place;
+	}
+
+	public void setPlace(String place) {
+		this.place = place;
+	}
+	
+	
+}
