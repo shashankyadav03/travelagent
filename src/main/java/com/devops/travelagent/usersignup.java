caeeb262885d4536c125e66cffa1265f72bfbed3
shashankyadav03 <shashankyadav97@yahoo.in>
2021-05-28 22:06:07 +0530

    Final commit

---
+++ src/main/java/com/devops/travelagent/usersignup.java
@@ -0,0 +1,38 @@
+package com.devops.travelagent;
+
+public class usersignup {
+	
+	String username;
+	String password;
+	String rpassword;
+	String email;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getRpassword() {
+		return rpassword;
+	}
+	public void setRpassword(String rpassword) {
+		this.rpassword = rpassword;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+
+}
